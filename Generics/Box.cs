9b05c1842dcebb42e3f34589a985d525606f0fb0
Teddy Aleksov <teddy57603@gmail.com>
2021-02-04 13:13:58 +0200

    Add project files.

---
+++ Generics/Box.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            Elements = new List<T>();
+        }
+        public Box(List<T> arr)
+            :this()
+        {
+            Elements = arr;
+        }
+        public List<T> Elements { get; set; }
+        public int Count => this.Elements.Count;
+
+        public void Add(T element)
+        {
+            Elements.Add(element);
+        }
+        
+        public T Remove()
+        {
+            T el = Elements[Elements.Count - 1];
+            Elements.RemoveAt(Elements.Count - 1);
+            return el;
+        }
+    }
+}
